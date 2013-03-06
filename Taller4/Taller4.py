4998f7bd8f247bdabe3c41dfa70d535e86b2ee84
ANDRES PENARANDA GARCIA <a.penaranda451@compufi13.uniandes.edu.co>
2013-03-06 13:07:38 -0500

    codigo con las graficas, falta arreglar el problema con pyplot

--- Taller4/Taller4.py
+++ Taller4/Taller4.py
@@ -1,7 +1,7 @@
 import os
 import numpy
 import sys
-import pylab
+import matplotlib.pyplot as plt
 import scipy
 from scipy.optimize import curve_fit
 from scipy.linalg import eigh
@@ -80,19 +80,42 @@ for ang in angulo:
        	
 
 # grafica gmedia vs theta y lo guarda en el archivo 'gmedia_vs_theta.png'
-pylab.plot(angulo, gmedia, '.')
-pylab.xlabel('the (degrees)')
-pylab.ylabel('gmedia (m/s^2)')
-pylab.title('gmedia vs Theta')
-pylab.savefig('gmedia_vs_theta')
-pylab.grid(True)
-pylab.show()
+plt.plot(angulo, gmedia, '.')
+plt.xlabel('the (degrees)')
+plt.ylabel('gmedia (m/s^2)')
+plt.title('gmedia vs Theta')
+plt.savefig('gmedia_vs_theta')
+plt.grid(True)
+plt.show()
 
 
 
-for g in gmedia:
-	F.append(1 -((g)/(9.81)))  
+for i in range(len(gmedia)):
+	F.append(1 -((gmedia[i])/(9.81)))  
 
+
+# hace un fit de los datos
+def fit(x, a, b):
+     return a*numpy.sin(numpy.deg2rad(x)) + b
+
+param = (scipy.optimize.curve_fit(fit, numpy.array(angulo), numpy.array(F)))[0]
+
+plt.plot(angulo, fit(angulo,param[0],param[1]), '.')
+plt.plot(angulo, F, '.')
+plt.xlabel('angulo (grados)')
+plt.ylabel('Variacion Gravitacional (adimensional)')
+plt.title('Variacion Gravitacional vs angulo')
+plt.grid(True)
+plt.show()
+
+# grafica residuo vs angulo y lo guarda en el archivo 'Residuo_vs_angulo.png'
+plt.plot(angulos, fit(angulos,parameters[0],parameters[1])-F, '.')
+plt.xlabel('angulo (grados)')
+plt.ylabel('residuo (adimensiona)')
+plt.title('Residuo vs angulo')
+plt.savefig('Residuo_vs_angulo.png')
+plt.grid(True)
+plt.show()
 
 	
 
