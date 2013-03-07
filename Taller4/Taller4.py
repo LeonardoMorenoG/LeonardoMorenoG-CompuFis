ef213034a90a6f58ec919187851f382d26580e65
ANDRES PENARANDA GARCIA <a.penaranda451@compufi17.uniandes.edu.co>
2013-03-07 09:26:03 -0500

    version final con graficas

--- Taller4/Taller4.py
+++ Taller4/Taller4.py
@@ -1,10 +1,11 @@
 import os
 import numpy
 import sys
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import scipy
 from scipy.optimize import curve_fit
 from scipy.linalg import eigh
+from numpy import linalg as LA
 
 
 table = []
@@ -13,7 +14,9 @@ var_gravity = []
 avg_grav = []
 pca = []
 data = open('data.dat', 'w')
-
+valF = open('valF.dat', 'w')
+residuoF = open('residuoF.dat', 'w')
+angulog = open('angulog.dat' ,'w')
 # corre un loop en el directorio hw4_data
 for filename in os.listdir('hw4_data/' ):
 
@@ -78,20 +81,56 @@ for ang in angulo:
        	cont /= 6.0
        	gmedia.append(cont)
 
-
+"""
 # grafica gmedia vs theta y lo guarda en el archivo 'gmedia_vs_theta.png'
 plt.plot(angulo, gmedia, '.')
-plt.xlabel('the (degrees)')
+plt.xlabel('theta (degrees)')
 plt.ylabel('gmedia (m/s^2)')
 plt.title('gmedia vs Theta')
 plt.savefig('gmedia_vs_theta')
 plt.grid(True)
 plt.show()
+"""
 
+# obtiene la matriz de covarianzas
+#1. Obtener el vector de los valores de la gravedad en cada exp.
+#2. Obtener el vector de los valores de la velocidad en x en cada exp.
+#3. Obtener el vector de los valores de la velocidad en y en cada exp.
+gravedad = []
+vSubx = []
+vSuby = []
+
+for row in table:
+    gravedad.append(row[4])
+    vSubx.append(row[2])
+    vSuby.append(row[3])
+
+M=numpy.vstack(((gravedad,vSubx),vSuby))
+Mcov = numpy.cov(M)
+print ("Matriz de Covarianzas: ")
+print numpy.cov(M)
+evals, evecs = LA.eig(Mcov)
+print ("Autovalores: ", evals)
+print ("Autovectores: ")
+print evecs
 
 for g in gmedia:
 	F.append(1 -((g)/(9.81)))  
 
+for i in range(len(angulo)):
+	
+	angulog.write('%f ' % (angulo[i]))
+	angulog.write( '%f ' % (gmedia[i]))
+       	angulog.write('\n')
+
+
+for i in range(len(F)):
+	
+	valF.write('%f ' % (angulo[i]))
+	valF.write( '%f ' % (F[i]))
+       	valF.write('\n')
+
+	
 
 
 # hace un fit de los datos
@@ -101,6 +140,13 @@ def fit(x, a, b):
 
 param = (scipy.optimize.curve_fit(fit, numpy.array(angulo), numpy.array(F)))[0]
 
+for i in range(len(F)):
+	
+	residuoF.write('%f ' % (angulo[i]))
+	residuoF.write( '%f ' % ( fit(angulo[i],param[0],param[1])-F[i]))
+       	residuoF.write('\n')
+
+"""
 plt.plot(angulo, fit(angulo,param[0],param[1]), '.')
 plt.plot(angulo, F, '.')
 plt.xlabel('angulo (grados)')
@@ -110,7 +156,7 @@ plt.grid(True)
 plt.show()
 
 # grafica residuo vs angulo y lo guarda en el archivo 'Residuo_vs_angulo.png'
-plt.plot(angulos, fit(angulos,parameters[0],parameters[1])-F, '.')
+plt.plot(angulos, fit(angulo,param[0],param[1])-F, '.')
 plt.xlabel('angulo (grados)')
 plt.ylabel('residuo (adimensiona)')
 plt.title('Residuo vs angulo')
@@ -118,8 +164,7 @@ plt.savefig('Residuo_vs_angulo.png')
 plt.grid(True)
 plt.show()
 
-
-
+"""
 
 	      
 
